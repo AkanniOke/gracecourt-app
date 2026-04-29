fd6fe55ec3e96cfa50eae42a0403d4459020f22a
Akanni Clement <akanniokelola@gmail.com>
2026-04-29 22:46:16 +0100

    Fix googleServicesFile env usage

---
+++ app.config.js
@@ -0,0 +1,73 @@
+const googleServicesFile =
+  process.env.GOOGLE_SERVICES_JSON || './android/app/google-services.json';
+
+module.exports = {
+  expo: {
+    name: 'gracecourt-app',
+    slug: 'gracecourt-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'gracecourtapp',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        UIBackgroundModes: ['audio'],
+      },
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./android/app/google-services.json",
+      adaptiveIcon: {
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './assets/images/android-icon-foreground.png',
+        backgroundImage: './assets/images/android-icon-background.png',
+        monochromeImage: './assets/images/android-icon-monochrome.png',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: 'com.gracecourt.globalapp',
+    },
+    web: {
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+          dark: {
+            backgroundColor: '#000000',
+          },
+        },
+      ],
+      'expo-sqlite',
+      'expo-audio',
+      [
+        'expo-notifications',
+        {
+          color: '#0A2E73',
+          defaultChannel: 'default',
+          icon: './assets/images/android-icon-monochrome.png',
+        },
+      ],
+      'expo-asset',
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: 'c6093b46-4b11-4bd9-b70a-72ec546e8a47',
+      },
+    },
+  },
+};
